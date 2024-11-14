693c2e41a599b0a133b119ff974fd4b2e3c46444
somewhat-armoured-shrimp <tor24017@byui.edu>
2024-11-14 15:16:56 -0800

    didnt know i had to push something yesterday hahahahhahhaha yesterday was bad okay

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,11 @@
+public class Entry
+{
+    public DateTime _date;
+    public string _prompt;
+    public string _response;
+
+    public void Display()
+    {
+
+    }
+}
---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,22 @@
+public class Journal
+{
+    public List<Entry> _entries;
+
+
+    public void Display()
+    {
+
+    }
+    public void AddEntry(Entry entry)
+    {
+
+    }
+    public void Save(string filename)
+    {
+
+    }
+    public void Load(string filename)
+    {
+        
+    }
+}
